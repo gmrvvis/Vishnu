dce213677f3f5da9ae5e65316b0b1b9b747ef93b
Gonzalo Bayo <gonzalobayo@gmail.com>
2017-05-16 10:41:44 +0200

    Avoid opening multiple instances of launcher

--- vishnu/main.cpp
+++ vishnu/main.cpp
@@ -1,7 +1,8 @@
 #include <QApplication>
 #include <QResource>
-//#include <QMessageBox>
-//#include <QSharedMemory>
+#include <QMessageBox>
+#include <QSharedMemory>
+#include <QSystemSemaphore>
 
 #include "MainWindow.h"
 
@@ -12,15 +13,40 @@ int main( int argc, char *argv[] )
   QApplication::setApplicationName( "Launcher" );
   QApplication::setOrganizationName( "GMRV - URJC" );
 
-  /*QSharedMemory sharedMemory;
-  sharedMemory.setKey( QApplication::applicationName() + "-Key" );
-  if( sharedMemory.create( 1 ) == false )
+  const QString sharedMemorykey = QApplication::applicationName( ) + QString( "-Key" );
+  const QString semaphoreKey = QApplication::applicationName( ) + QString( "-SemKey" );
+
+  QSystemSemaphore semaphore( semaphoreKey, 1 );
+  semaphore.acquire();
+
+#ifdef __linux__
+  // On unix, shared memory is not freed upon crash
+  QSharedMemory unixSharedMemory( sharedMemorykey );
+  bool detach = false;
+  if ( unixSharedMemory.attach( ) == false )
+  {
+    detach = unixSharedMemory.detach( );
+  }
+  unixSharedMemory.detach( );
+
+#endif
+
+  QSharedMemory sharedMemory( sharedMemorykey );
+  bool running = sharedMemory.attach( );
+  if ( !running )
+  {
+    sharedMemory.create( 1 );
+  }
+
+  semaphore.release( );
+
+  if( running )
   {
     QMessageBox::warning( NULL, "Warning!", "Another instance of " +
       QApplication::applicationName( ) + " is already running!" );
     app.exit( );
-    return 0;
-  }*/
+    return 1;
+  }
 
   vishnu::MainWindow window;
   QResource::registerResource( "resources.rcc" );
